b0d13850e28214f55dcaed0bed2e13a690dad778
teresav3 <tvargin@gmail.com>
2025-04-07 03:00:10 +0000

    committed

---
+++ Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        //Part 1
+        int a = 10, b = 5, c = 15;
+        
+
+    }
+}
